1439d2649546a3739ee990b3ac94a031b6b882fa
Armour <497052684@qq.com>
2015-04-06 16:53:48 +0800

    change programming style

--- app.js
+++ app.js
@@ -6,36 +6,37 @@ var express = require('express');
 var app = express();
 
 dbclient = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "travel36512",
-  database: "zblibrary"
+	host: "localhost",
+	user: "root",
+	password: "travel36512",
+	database: "zblibrary"
 });
 
 app.use(bodyParser());
 
-app.get("/", function(req, res){
-  var realpath = __dirname + "/public/main.html";
+app.get("/", function(req, res) {
+	var realpath = __dirname + "/public/main.html";
 	res.sendFile(realpath);
 })
 
 app.get('*.*', function(req, res) {
-  var realpath = __dirname + req.url;
-  res.sendFile(realpath);
+	var realpath = __dirname + req.url;
+	res.sendFile(realpath);
 });
 
 app.post("/show", function(req, res) {
-  var queryString = "";
-  queryString = 'SELECT square FROM zbl WHERE py="' + req.body.py +'" ORDER BY code';
-  dbclient.query(queryString, function(err, results, fields) {
-    //console.log(results);
-    res.send(results);
-  });
+	var queryString = "";
+	queryString = 'SELECT square FROM zbl WHERE py="' + req.body.py + '" ORDER BY code';
+	dbclient.query(queryString, function(err, results, fields) {
+		//console.log(results);
+		res.send(results);
+	});
 })
 
-dbclient.connect(function(err, results){
-  console.log("mysql has started");
+dbclient.connect(function(err, results) {
+	console.log("mysql has started");
 });
+
 var server = app.listen(2333, function() {
 	console.log("Server has started.");
 })
